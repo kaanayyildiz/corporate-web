419c64140ea180428d075cffc8dd3cb9a8ca9a7f
Kaan Ayyıldız <ykaanayyildiz@gmail.com>
2024-03-12 17:02:52 +0300

    updates

--- app/components/layout/Sectors.tsx
+++ app/components/layout/Sectors.tsx
@@ -3,12 +3,40 @@ import React from "react";
 const Sectors = () => {
   return (
     <div className="flex flex-col min-h-max mx-auto p-7 md:p-32 border-b">
-      <h1 className="font-thin pb-2 text-2xl md:text-4xl lg:text-5xl">
-       Our sectors
+      <h1 className="font-thin pb-2 text-2xl md:text-4xl lg:text-5xl mb-16">
+        Our sectors
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <div>1</div>
-        <div>2</div>
+        <div className="flex flex-col gap-5">
+          <svg
+            className="w-24"
+            fill="#000000"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path d="M604.16 788.48H419.84V947.2c0 14.138-11.462 25.6-25.6 25.6s-25.6-11.462-25.6-25.6V762.88c0-14.138 11.462-25.6 25.6-25.6h235.52c14.138 0 25.6 11.462 25.6 25.6V947.2c0 14.138-11.462 25.6-25.6 25.6s-25.6-11.462-25.6-25.6V788.48zm385.033 130.041c14.138 0 25.6 11.462 25.6 25.6s-11.462 25.6-25.6 25.6h-80.475c-14.138 0-25.6-11.462-25.6-25.6V347.279a5.12 5.12 0 00-5.12-5.12H530.98c-25.449 0-46.08-20.631-46.08-46.08V123.205a5.12 5.12 0 00-5.12-5.12H141.694a5.12 5.12 0 00-5.12 5.12V946.48c0 14.138-11.462 25.6-25.6 25.6H27.521c-14.138 0-25.6-11.462-25.6-25.6s11.462-25.6 25.6-25.6h57.853V123.205c0-31.105 25.215-56.32 56.32-56.32H479.78c31.105 0 56.32 25.215 56.32 56.32v167.754h341.898c31.105 0 56.32 25.215 56.32 56.32v571.242h54.875zM220.16 174.08h184.32c14.138 0 25.6 11.462 25.6 25.6s-11.462 25.6-25.6 25.6H220.16c-14.138 0-25.6-11.462-25.6-25.6s11.462-25.6 25.6-25.6zm0 112.64h184.32c14.138 0 25.6 11.462 25.6 25.6s-11.462 25.6-25.6 25.6H220.16c-14.138 0-25.6-11.462-25.6-25.6s11.462-25.6 25.6-25.6zm0 112.64h583.68c14.138 0 25.6 11.462 25.6 25.6s-11.462 25.6-25.6 25.6H220.16c-14.138 0-25.6-11.462-25.6-25.6s11.462-25.6 25.6-25.6zm0 112.64h583.68c14.138 0 25.6 11.462 25.6 25.6s-11.462 25.6-25.6 25.6H220.16c-14.138 0-25.6-11.462-25.6-25.6s11.462-25.6 25.6-25.6zm0 112.64h583.68c14.138 0 25.6 11.462 25.6 25.6s-11.462 25.6-25.6 25.6H220.16c-14.138 0-25.6-11.462-25.6-25.6s11.462-25.6 25.6-25.6zm0 112.64h71.68c14.138 0 25.6 11.462 25.6 25.6s-11.462 25.6-25.6 25.6h-71.68c-14.138 0-25.6-11.462-25.6-25.6s11.462-25.6 25.6-25.6zm512 0h71.68c14.138 0 25.6 11.462 25.6 25.6s-11.462 25.6-25.6 25.6h-71.68c-14.138 0-25.6-11.462-25.6-25.6s11.462-25.6 25.6-25.6zm-512 122.88h71.68c14.138 0 25.6 11.462 25.6 25.6s-11.462 25.6-25.6 25.6h-71.68c-14.138 0-25.6-11.462-25.6-25.6s11.462-25.6 25.6-25.6zm512 0h71.68c14.138 0 25.6 11.462 25.6 25.6s-11.462 25.6-25.6 25.6h-71.68c-14.138 0-25.6-11.462-25.6-25.6s11.462-25.6 25.6-25.6z"></path>
+            </g>
+          </svg>
+          <h1 className="text-lg md:text-xl lg:text-2xl">Buildings</h1>
+          <p className="max-w-md text-lg">
+            We manufacture and process stainless steel components using modern
+            production techniques and high-quality materials.
+          </p>
+        </div>
+        <div>
+          <h1 className="text-lg md:text-xl lg:text-2xl">Buildings</h1>
+          <p className="max-w-md text-lg">
+            We manufacture and process stainless steel components using modern
+            production techniques and high-quality materials.
+          </p>
+        </div>
         <div>3</div>
       </div>
     </div>
